ddcb1460742193172ab8e7e4b75b23b50f3c1c5a
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-09-17 15:26:09 +1000

    Implemented the socket client

---
+++ client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>		/* standard I/O routines */
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>	/* inet_addr */
+
+int main(int argc, char** argv) {
+
+	int socket_desc;
+	struct sockaddr_in server;
+
+	/* craete socket */
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (socket_desc == -1) {
+		puts("Could not create socket");
+	}
+	puts("Socket created");
+
+	/* configuring end point */
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
+
+	/* connect to remote server */
+	if (connect(socket_desc, (struct sockaddr *)&server,
+		sizeof(server)) < 0) {
+		perror("connect");
+		return 1;
+	}
+	puts("Connected\n");
+
+	while (1) {
+		
+	}
+
+	// close(socket_desc);
+
+	return 0;
+}
